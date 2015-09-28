21be7a2174b709d65380755cdfe70ae8cff69a4e
wave100 <rshadra@gmail.com>
2015-09-28 18:31:38 -0400

    GroupMailer

--- src/java/me/rishshadra/gmailer/GroupMailer.java
+++ src/java/me/rishshadra/gmailer/GroupMailer.java
@@ -5,20 +5,55 @@
  */
 package me.rishshadra.gmailer;
 
+import java.io.IOException;
+import javax.mail.MessagingException;
+import me.rishshadra.nhstracker.Student;
+
 /**
  *
  * @author Rish Shadra <rshadra@gmail.com>
  */
 public class GroupMailer {
-    public GroupMailer(String s) {
-        if (s.contains("$NAME") && s.contains("$FULL_NAME") && s.contains("$PIN") && s.contains("$LINK")) {
-            s.replaceAll("$NAME", "nametest");
-            s.replaceAll("$FULL_NAME", "fullnametest");
-            s.replaceAll("$PIN", "pintest");
-            s.replaceAll("$LINK", "linktest");
-        }
-        
-        System.out.println(s);
-        
+
+    private final String message;
+    private final String subject;
+    private String preparedMessage;
+    private Student student;
+    private final GMailer mailer = new GMailer();
+
+    public GroupMailer(String m, String s) {
+        message = m;
+        subject = s;
     }
+
+    public void prepareMessage(Student s, String link) {
+        student = s;
+        preparedMessage = message;
+        String fullName = s.getName();
+        String pin = String.valueOf(s.getPIN());
+
+        if (preparedMessage.contains("$NAME") && preparedMessage.contains("$FULL_NAME") && preparedMessage.contains("$PIN") && preparedMessage.contains("$LINK")) {
+            preparedMessage = preparedMessage.replace("$NAME", fullName.substring(0, fullName.indexOf(" ")));
+            preparedMessage = preparedMessage.replace("$FULL_NAME", fullName);
+            preparedMessage = preparedMessage.replace("$PIN", pin);
+            preparedMessage = preparedMessage.replace("$LINK", link);
+        } else {
+            System.out.println("Malformed Input: " + preparedMessage.contains("$NAME") + preparedMessage.contains("$FULL_NAME") + preparedMessage.contains("$PIN") + preparedMessage.contains("$LINK"));
+        }
+        me.rishshadra.nhstracker.logging.Logger.logText("Prepared message for " + s.getName());
+    }
+    
+    public void printPreparedMessage() {
+        System.out.println(preparedMessage);
+    }
+    
+    public String getPreparedMessage() {
+        return preparedMessage;
+    }
+    
+    public void sendPreparedMessage() throws MessagingException, IOException {
+        me.rishshadra.nhstracker.logging.Logger.logText("Sent message to " + student.getName() + " (" + student.getEmail() + ")");
+        mailer.sendMessage(student.getEmail(), subject, preparedMessage);
+    }
+    
 }
