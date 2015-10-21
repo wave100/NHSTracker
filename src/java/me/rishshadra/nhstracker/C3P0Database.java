6a4c08390ddd3ba59b22d57653815cf072b87aac
wave100 <rshadra@gmail.com>
2015-10-21 00:22:27 -0400

    Configured C3P0 pooling.

--- src/java/me/rishshadra/nhstracker/C3P0Database.java
+++ src/java/me/rishshadra/nhstracker/C3P0Database.java
@@ -6,15 +6,11 @@
 package me.rishshadra.nhstracker;
 
 import com.mchange.v2.c3p0.ComboPooledDataSource;
-import com.mchange.v2.c3p0.DataSources;
 import java.beans.PropertyVetoException;
 import java.sql.Connection;
 import java.sql.SQLException;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.sql.DataSource;
 
 /**
  *
@@ -25,15 +21,21 @@ public class C3P0Database extends Database {
     static ComboPooledDataSource finalSource;
 
     static {
+        System.out.println("C3P0 Pool Initializing..");
         try {
             ComboPooledDataSource c3combo = new ComboPooledDataSource();
             c3combo.setDriverClass("com.mysql.jdbc.Driver");
             c3combo.setJdbcUrl("jdbc:mysql://localhost:3306/nhsdb");
             c3combo.setUser("root");
             c3combo.setPassword("");
-            c3combo.setMinPoolSize(20);
-            c3combo.setMaxPoolSize(100);
+            c3combo.setMinPoolSize(5);
+            c3combo.setInitialPoolSize(10);
+            c3combo.setMaxPoolSize(15);
             c3combo.setAutomaticTestTable("c3test");
+            c3combo.setMaxIdleTime(300);
+            c3combo.setMaxConnectionAge(600);
+            c3combo.setUnreturnedConnectionTimeout(360);
+            c3combo.setDebugUnreturnedConnectionStackTraces(true);
             c3combo.setTestConnectionOnCheckin(true);
             c3combo.setTestConnectionOnCheckout(true);
 
