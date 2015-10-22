f52b8b3d58f55205dc08e3f8c87f0b3bbc764de6
wave100 <rshadra@gmail.com>
2015-10-22 16:32:13 -0400

    Added templates for token system, fixed an unclosed connection in StudentMatcher.

--- src/java/me/rishshadra/nhstracker/StudentMatcher.java
+++ src/java/me/rishshadra/nhstracker/StudentMatcher.java
@@ -77,6 +77,9 @@ public class StudentMatcher {
                 }
             }
         }
+        
+        r.close();
+        
         return -1;
     }
 }
---
+++ src/java/me/rishshadra/nhstracker/tokens/TokenChecker.java
@@ -0,0 +1,14 @@
+/*
+ * Written by Rish Shadra.
+ * There's really not much more to this license header than that.
+ * 
+ */
+package me.rishshadra.nhstracker.tokens;
+
+/**
+ *
+ * @author Rish
+ */
+public class TokenChecker {
+    
+}
